9a5e1e83e08c3d4a2df5feac4b0b52dd58c0acf8
squirrelkit <kayapeiman@gmail.com>
2023-11-08 17:48:09 -0400

    A cool project, have yet to figure out WHY my letter is sideways

---
+++ sketches/sketch-05.js
@@ -0,0 +1,162 @@
+const canvasSketch = require('canvas-sketch');
+// const { convertToSVGPath } = require('canvas-sketch-util/penplot');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+let manager;
+
+let text = 'R';
+let fontSize = 1200;
+let fontFamily = 'Serif';
+
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width  = cols;
+  typeCanvas.height = rows;
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
+
+    fontSize = cols;
+
+    typeContext.fillStyle = 'white';
+    typeContext.font = `${fontSize}px ${fontFamily}`; // "1200px Gabriola"
+    typeContext.textBaseline = 'top';
+
+    const metrics = typeContext.measureText(text);
+    const mX = metrics.actualBoundingBoxLeft * -1;
+    const mY = metrics.actualBoundingBoxAscent * -1;
+    const mW = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mH = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const tx = (cols  - mW) * .5 - mX;
+    const ty = (rows - mH) * 0.5 - mY;
+
+    typeContext.save();
+    
+    // const canvas = document.querySelector('#canvas')
+    
+    const centerX = cols / 2,
+    centerY = rows / 2;
+
+    
+    // typeContext.translate();
+    // typeContext.scale(1, -1);
+    typeContext.beginPath();
+    typeContext.translate(tx, ty);
+    // typeContext.translate(centerX, centerY);
+    typeContext.rect(mX, mY, mW, mH);
+    typeContext.rotate(-1 * Math.PI / 118);
+    typeContext.stroke();
+    // typeContext.transform()
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+    // typeContext.scale(-1, -1);
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    // context.drawImage(typeCanvas, 0, 0);
+  
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = row * cell;
+      const y = col * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      const glyph = getGlyph(r);
+
+      context.font = `${cell * 2}px ${fontFamily}`;
+
+      context.fillStyle = `white`;
+      
+      context.save();
+      context.translate(x, y);
+      context.translate(cell * 0.5, cell * 0.5);
+      // context.fillRect(0, 0, cell, cell);
+
+      // context.beginPath();
+      // context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
+      // context.fill();
+
+      context.fillText(glyph, 0, 0);
+
+      context.restore();  
+
+    }
+  };
+};
+
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '-';
+  if (v < 150) return '+';
+  if (v < 200) return '=';
+
+  return text;
+}
+
+const onKeyUp = (e) =>{
+  text = e.key.toUpperCase(); 
+  manager.render();
+}
+
+document.addEventListener('keyup', onKeyUp);
+
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+}
+
+start();
+
+
+
+
+/*
+const url = 'https://picsum.photos/200';
+
+const loadMeSomeImage = (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.src = url;
+  });
+}
+
+const start = async () => {
+  const img = await loadMeSomeImage(url);
+  console.log('image width',img.width);
+  console.log('this line');
+};
+
+//const start = () => {
+//  loadMeSomeImage(url).then(img => {
+//    console.log('image width',img.width);
+//  });
+//  console.log('this line');
+//};
+
+start();
+
+*/
